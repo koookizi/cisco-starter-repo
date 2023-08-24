15dcfe74258898fc1e02fe0b5f2b649ddc5ccce1
koookizi <koookizi@outlook.com>
2023-08-24 12:55:42 +0100

    Task 4

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -3,20 +3,25 @@ import Banner from './components/banner'
 import Exhibit from './components/exhibit'
 import IPAPI4 from './components/ipapi4'
 import IPAPI6 from './components/ipapi6'
+import Latency from './components/latency'
 
 
 function App() {
   return (
       <div className="App">
           <Banner bannerText="Sextant" />
-          <div className="ip_addresses">
+          <div className="flex-boxes">
               <div className="ip_address">
                   <Exhibit name="Your Public IPv4 Address" children={<IPAPI4/>}></Exhibit>
               </div>
               <div className="ip_address">
                   <Exhibit name="Your Public IPv6 Address" children={<IPAPI6/>}></Exhibit>
               </div>
+              <div className="latency">
+                  <Exhibit name="Latency" children={<Latency/>}></Exhibit>
+              </div>
           </div>
+
       </div>
   );
 }
--- sextant/src/components/exhibit.js
+++ sextant/src/components/exhibit.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import '../styles/Exhibit.css'
 
 
 export default class Exhibit extends Component {
